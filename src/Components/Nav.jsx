6d187b9aada284a6e5afae7e9d51004236a8780c
Pratik1805 <pratikpandey0532@gmail.com>
2024-04-28 12:16:16 +0530

    Fixed bugs

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -29,9 +29,9 @@ const Nav = () => {
       initial="initial"
       animate="animate"
     >
-      <motion.div className="md:flex items-center justify-evenly py-4 md:px-10 px-7 w-full">
+      <motion.div className="md:flex items-center justify-evenly py-4 md:px-10 px-7 w-full gap-4">
         <Link to="/">
-          <motion.div className=" flex justify-start items-center">
+          <motion.div className=" flex justify-start items-center sm:w-full w-[80%]">
             <motion.div className=" flex items-center justify-center bg-white rounded-full w-[50px] h-[50px]  px-2 mx-4 py-4">
               <motion.img src={gaming} alt="icon" width={76} height={46} />
             </motion.div>
@@ -39,14 +39,14 @@ const Nav = () => {
               Game Central
             </motion.h1>
           </motion.div>
-
-          <motion.div
-            onClick={() => setOpen(!open)}
-            className="absolute right-8 top-6 cursor-pointer md:hidden"
-          >
-            <img src={hamburger} alt="Hamburger" width={36} height={36} />
-          </motion.div>
         </Link>
+
+        <motion.div
+          onClick={() => setOpen(!open)}
+          className="absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          <img src={hamburger} alt="Hamburger" width={36} height={36} />
+        </motion.div>
         <motion.ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static gap-8 sm:bg-primary bg-white max-sm:rounded-md md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             open ? "top-20 " : "top-[-490px]"
@@ -65,6 +65,7 @@ const Nav = () => {
               </a>
             </motion.li>
           ))}
+          <div className=" flex items-start justify-center gap-4 flex-col sm:flex-row">
           <motion.div>
             <Link to="/about">
               <Button label="About Us" />
@@ -75,6 +76,7 @@ const Nav = () => {
               <Button label="Contact Us" />
             </Link>
           </motion.div>
+          </div>
         </motion.ul>
       </motion.div>
     </motion.div>
